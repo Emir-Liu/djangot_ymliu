9951f5aefce44140a91c6e3984073bf6bc06dbb3
Emir-Liu <929555739@qq.com>
2024-05-28 08:56:15 +0800

    建立model和admin

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -27,10 +27,12 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-
 # Application definition
 
 INSTALLED_APPS = [
+    'blog',
+    'config',
+    'comment',
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
--- django_blog/manage.py
+++ django_blog/manage.py
@@ -6,7 +6,9 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "django_blog.settings")
+    # os.environ.setdefault("DJANGO_SETTINGS_MODULE", "django_blog.settings")
+    profile = os.environ.get('TYPEIDEA_PROFILE', 'develop')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE','django_blog.settings.%s' % profile)
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
