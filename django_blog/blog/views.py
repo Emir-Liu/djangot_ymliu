81c35f0749d75c62bb0b20d31df5d3bb64d59336
Emir-Liu <929555739@qq.com>
2024-06-16 12:49:55 +0800

    基础的功能已经完成

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,56 +1,120 @@
-from django.shortcuts import render
-
-from .models import Post, Tag, Category
+from django.shortcuts import get_object_or_404
+from django.views.generic import DetailView, ListView
 
 from config.models import SideBar
-
-# from django.http import HttpResponse
-# Create your views here.
-
-def post_list(request, category_id=None, tag_id=None):
-    tag = None
-    category = None
-
-    if tag_id:
-        post_list, tag = Post.get_by_tag(tag_id = tag_id)
-    elif category_id:
-        post_list, category = Post.get_by_category(category_id = category_id)
-    else:
-        post_list = Post.latest_posts()
+from .models import Post, Category, Tag
 
 
-    context = {
-        'category': category,
-        'tag': tag,
-        'post_list': post_list,
-        'sidebars': SideBar.get_all()
-    }
+class CommonViewMixin:
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context.update(
+            {
+                'sidebars': SideBar.get_all(),
+            }
+        )
+        context.update(
+            Category.get_navs()
+        )
+        return context
 
 
+class IndexView(CommonViewMixin, ListView):
+    queryset = Post.latest_posts()
+    paginate_by = 5
+    context_object_name = 'post_list'
+    template_name = 'blog/list.html'
 
-    return render(
-        request,
-        'blog/list.html',
-        context=context
-    )
 
-def post_detail(request, post_id):
+class CategoryView(IndexView):
+    def get_context_data(self, **kwargs):
+        '''
+        获取到Category相关的参数
 
-    try:
-        post = Post.objects.get(id=post_id)
-    except Post.DoesNotExist:
-        post = None
+        Args:
+            **kwargs:
 
-    context = {
-        'post': post,
-        'sidebars': SideBar.get_all()
-    }
+        Returns:
 
-    context.update(Category.get_navs())
+        '''
+        context = super().get_context_data(**kwargs)
+        category_id = self.kwargs.get('category_id')
+        category = get_object_or_404(Category, pk=category_id)
+        context.update(
+            {
+                'category': category
+            }
+        )
 
-    return render(
-        request,
-        'blog/detail.html',
-        context = context
-    )
+        return context
 
+    def get_queryset(self):
+        '''
+        对获取到的数据，通过category进行过滤
+
+        Returns:
+
+        '''
+        queryset = super().get_queryset()
+        category_id = self.kwargs.get('category_id')
+        return queryset.filter(category_id=category_id)
+
+
+class TagView(IndexView):
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        tag_id = self.kwargs.get('tag_id')
+        tag = get_object_or_404(
+            Tag,
+            pk=tag_id
+        )
+        context.update(
+            {
+                'tag': tag,
+            }
+        )
+
+        return context
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        tag_id = self.kwargs.get('tag_id')
+        return queryset.filter(tag_id=tag_id)
+
+
+class PostDetailView(CommonViewMixin, DetailView):
+    queryset = Post.latest_posts()
+    template_name = 'blog/detail.html'
+    context_object_name = 'post'
+    pk_url_kwarg = 'post_id'
+
+
+from django.db.models import Q
+
+
+class SearchView(IndexView):
+    def get_context_data(self):
+        context = super().get_context_data()
+        context.update(
+            {
+                'keyword': self.request.GET.get('keyword', '')
+            }
+        )
+        return context
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        keyword = self.request.GET.get('keyword')
+
+        if not keyword:
+            return queryset
+
+        return queryset.filter(Q(title_icontains=keyword) | Q(desc_icontains=keyword))
+
+
+class AuthorView(IndexView):
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        author_id = self.kwargs.get('owner_id')
+        return queryset.filter(owner_id=author_id)
